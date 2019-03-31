1b48971899fa467ae57a476cbfb35d469a817e21
김성훈 <seonghun127@gmail.com>
2019-03-31 17:14:52 +0900

    first commit

---
+++ server/src/main/java/com/ssns/SsnsApplication.java
@@ -0,0 +1,13 @@
+package com.ssns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsnsApplication.class, args);
+    }
+
+}
